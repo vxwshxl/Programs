1283f8f8efea010cc4005df44a0fdc95bced10fc
vxwshxl <veebodosa@gmail.com>
2026-03-20 11:20:11 +0530

    32.cpp

--- c++/33.cpp
+++ c++/33.cpp
@@ -1,29 +1,31 @@
-33.write a program in c++ to input a number and check whether it is a prime number or not
+// 33. Write a program in c++ to input a number and check whether it is a prime number or not.
+
 #include<iostream>
 using namespace std;
-main()
-{
-int n,i;
-bool f;
-f=false;
-cout<<endl<<"Enter a term:";
-cin>>n;
-i=2;
-while(i<n)
-{
-if(n%i==0)
-{
-f=true;
+
+int main() {
+    int n,i;
+    bool f;
+    f=false;
+    cout<<endl<<"Enter a term:";
+    cin>>n;
+    i=2;
+    while(i<n) {
+        if(n%i==0) {
+            f=true;
+        }
+        i++;
+    }
+    
+    if(f)
+        cout<<n<<"is not a prime number:";
+    else {
+        cout<<n<<"is a prime number:";
+    }
 }
-i++;
-}
-if(f)
-cout<<n<<"is not a prime number:";
-else
-{
-cout<<n<<"is a prime number:";
-}
-}
+
+
+// Output:
 Enter a term:6
 6is not a prime number:
 fingers@fingers-B460M-GAMING-HD:$ ./a.out
