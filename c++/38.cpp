a0ad1ede9b1abc8091109bcaaa0d8b115af921fb
vxwshxl <veebodosa@gmail.com>
2026-03-27 17:48:45 +0530

    38.cpp

--- c++/38.cpp
+++ c++/38.cpp
@@ -1,23 +1,24 @@
-38.write a program in c++ to input a string in lowercase character and convert it to uppercase character
+// 38. Write a program in c++ to input a string in lowercase character and convert it to uppercase character.
+
 #include <iostream>
 #include <cstring>
 using namespace std;
-main()
-{
+
+int main() {
    char ch[40]; int i,p;
    cout<<"Enter a character in lowercase: ";
    cin.getline(ch,40);
-p=strlen(ch);
-for(i=0;i<=p;i++)
-{
-if(ch[i]>=97 && ch[i]<=122)
-{
-ch[i]=ch[i]-32;
-}
-}
+   p=strlen(ch);
+   
+   for(i=0;i<=p;i++) {
+        if(ch[i]>=97 && ch[i]<=122) {
+            ch[i]=ch[i]-32;
+        }
+   }
    cout<<"Entered character in uppercase: "<<ch;
-
 }
-output:
+
+
+// Output:
 Enter a character in lowercase: rahul das
 Input Character in uppercase: RAHUL DAS
