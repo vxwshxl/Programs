be4981f7fecaf4c8a23291b42cd2327152dccea4
vxwshxl <veebodosa@gmail.com>
2026-03-27 17:47:51 +0530

    37.cpp

--- c++/37.cpp
+++ c++/37.cpp
@@ -1,31 +1,35 @@
-37.write a constructor in c++ to compare the two input string and compare it using strcmp() where output of the both input string is highlighted in bold letter.
+// 37. Write a constructor in c++ to compare the two input string and compare it using strcmp() where output of the both input string is highlighted in bold letter.
+
 #include <cstring>
 #include <iostream>
 #define b "\e[1m"
 #define cb "\e[0m"
 using namespace std;
-class stringcom
-{char str1[30];// = "Megadeth";
-  char str2[30];// = "Metallica";
-  public:
-stringcom();
+
+class stringcom {
+    char str1[30];// = "Megadeth";
+    char str2[30];// = "Metallica";
+    public:
+        stringcom();
 };
-stringcom::stringcom()
-{
-cout<<"Enter a string:";
-  cin.getline(str1,30);
-  cout<<"Enter another string:";
-  cin.getline(str2,30);
-   int result;
-  result = strcmp(str1, str2);
 
-  cout << "Comparing " <<b<< str1 <<cb<< " and " << b<<str2<<cb << ": " <<result << endl;
+stringcom::stringcom() {
+    cout<<"Enter a string:";
+    cin.getline(str1,30);
+    cout<<"Enter another string:";
+    cin.getline(str2,30);
+    int result;
+    result = strcmp(str1, str2);
+
+    cout << "Comparing " <<b<< str1 <<cb<< " and " << b<<str2<<cb << ": " <<result << endl;
 }
- main() {
-stringcom s1;  
- }
 
-output:
+int main() {
+    stringcom s1;  
+}
+
+
+// Output:
 Enter a string:pranab
 Enter another string:arnab
 Comparing pranab and arnab: 15
