7b7f855c476d545d85f0c717ea1b92dc10953d83
vxwshxl <veebodosa@gmail.com>
2026-05-08 11:12:38 +0530

    47.cpp

---
+++ c++/47.cpp
@@ -0,0 +1,49 @@
+// 47. Write a program in c++ to input a term and print the the following pattern using virtual base class
+/*
+n=5
+*****
+****
+***
+**
+*
+*/
+
+#include <iostream>
+using namespace std;
+class shape1 {
+    public:
+        int i,j,n;
+        shape1() {
+            cout<<"Enter the number of Rows:";
+            cin>>n;
+            for(i=n;i>=1;i--) {
+                for(j=1;j<=i;j++) {
+                    cout<<"* ";
+                }
+                cout<<endl;
+            }
+        }
+};
+class shape2 : public virtual shape1 {
+};
+class shape3 : public virtual shape1 {
+};
+class shape4 : public shape2, public shape3 {
+};
+ main()
+{
+	shape4 s4;  
+	cout << "Total term=" << s4.n << endl; 	
+}
+
+
+// Output:
+Enter the number of Rows:7
+* * * * * * *
+* * * * * *
+* * * * *
+* * * *
+* * *
+* *
+*
+Total term=7
