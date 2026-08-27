abb3031483c86e0c7def3cdf3352e61adc80b47b
vxwshxl <veebodosa@gmail.com>
2026-08-27 15:03:47 +0530

    DS - v3

--- DS/1.py
+++ DS/1.py
@@ -4,18 +4,18 @@ import pandas as pd
 import numpy as np
 
 # Import the original CSV dataset
-file_path = "/Users/vee/Downloads/products-100.csv"
+file_path = "1-100.csv"
 df = pd.read_csv(file_path)
 
 # Export datasets
-df.to_csv('/Users/vee/Downloads/data.csv', index=False)
-df.to_json('/Users/vee/Downloads/data.json', orient='records')
-df.to_excel('/Users/vee/Downloads/data.xlsx', index=False)
+df.to_csv('data.csv', index=False)
+df.to_json('data.json', orient='records')
+df.to_excel('data.xlsx', index=False)
 
 # Import datasets
-df_csv = pd.read_csv('/Users/vee/Downloads/data.csv')
-df_json = pd.read_json('/Users/vee/Downloads/data.json')
-df_excel = pd.read_excel('/Users/vee/Downloads/data.xlsx')
+df_csv = pd.read_csv('data.csv')
+df_json = pd.read_json('data.json')
+df_excel = pd.read_excel('data.xlsx')
 
 print("CSV Data:\n", df_csv)
 print("\nJSON Data:\n", df_json)
